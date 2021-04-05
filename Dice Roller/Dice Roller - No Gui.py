713d3ef8d3a89b9fe6b887748d238585c8b6df6e
Thaynan F. Freitas <65410847+thaynanff@users.noreply.github.com>
2021-04-05 01:40:28 -0300

    Create Dice Roller - No Gui.py

---
+++ Dice Roller/Dice Roller - No Gui.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+def dice_face(dice):
+    switch = {
+        1: '[       ]\n|   *   |\n[       ]\n',
+        2: '[*      ]\n|       |\n[      *]\n',
+        3: '[*      ]\n|   *   |\n[      *]\n',
+        4: '[*     *]\n|       |\n[*     *]\n',
+        5: '[*     *]\n|   *   |\n[*     *]\n',
+        6: '[ *   * ]\n| *   * |\n[ *   * ]\n'
+    }
+    return switch.get(dice)
+
+
+while True:
+    dice = randint(1, 6)
+    print(dice_face(dice))
+    ans = str(input('Would you like to roll the dice again? (Y/N): ').upper()).split()[0]
+    while ans not in 'YN':
+        ans = str(input('Invalid answer! Would you like to roll the dice again? (Y/N): ').upper()).split()[0]
+    if ans == 'N':
+        break
